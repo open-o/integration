8ccddfd0d971334f9baa3dc9d3e89c13701c3b6d
Gary Wu <gary.i.wu@huawei.com>
2016-09-08 07:05:03 -0700

    Add in repos that can compile successfully
    Change-Id: I9143009f6f3c8ef6f2ea7f8f2bc4408e4ec62490
    Issue-id: INT-21
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- autorelease/scripts/clone-all.sh
+++ autorelease/scripts/clone-all.sh
@@ -9,6 +9,7 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 while read p; do
+    rm -r $BUILD_DIR/$p
     #TODO: replace with https once repo is open to public
     git clone ssh://gwu@gerrit.open-o.org:29418/$p
 done < $ROOT/all-projects.txt
