9e920d29c09e804c6e323f8c28cdf505c1afc28e
Gary Wu <gary.i.wu@huawei.com>
2016-09-22 10:32:52 -0700

    Update docker image for common-tosca-catalog
    Change-Id: I0922c172dc1572b1f80660ea28e2aa5626efb46d
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-tosca-catalog/docker-entrypoint.sh
+++ test/csit/docker/common-tosca-catalog/docker-entrypoint.sh
@@ -4,8 +4,11 @@
 su mysql -c /usr/bin/mysqld_safe &
 sleep 2
 
+# Set mysql root password
+/usr/bin/mysqladmin -u root password 'rootpass'
+
 # Initialize DB schema
-mysql -uroot < dbscripts/mysql/openo-common_tosca-catalog-createobj.sql
+/service/initDB.sh root rootpass 3306 127.0.0.1
 
 # Start service
 /service/run.sh
