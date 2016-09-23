5161fec585adb6df4b90a4dcf0f6790a949777ef
Gary Wu <gary.i.wu@huawei.com>
2016-09-23 09:29:03 -0700

    Update docker image for MSB
    Change-Id: Ic77c7bbf2d2b5ad817c3da1c2a4a2f41ca91230d
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-services-microservice-bus/docker-entrypoint.sh
+++ test/csit/docker/common-services-microservice-bus/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/service/startup.sh
-tail -F /service/openresty/nginx/logs/error.log &
-tail -F /service/eag/nginx/logs/error.log &
-tail -F /service/apiroute-works/logs/application.log
+./startup.sh
+tail -F ./openresty/nginx/logs/error.log &
+tail -F ./eag/nginx/logs/error.log &
+tail -F ./apiroute-works/logs/application.log
