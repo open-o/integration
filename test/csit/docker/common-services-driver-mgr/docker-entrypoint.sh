dfadb92e94d49829a01d9af5622bc186c600a378
Gary Wu <gary.i.wu@huawei.com>
2016-09-23 12:46:58 -0700

    Add docker image for common-services-driver-mgr
    Change-Id: Ia89c590299f95cdbda07d0950baca88a4fbc49a0
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/common-services-driver-mgr/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$MSB_ADDR" ]; then
+    echo "Missing required variable MSB_ADDR: Microservices Service Bus address <ip>:<port>"
+    exit 1
+fi
+
+# Configure MSB IP address
+sed -i "s|msb.address=.*|msb.address=$MSB_ADDR|" etc/microservice.ini
+cat etc/microservice.ini
+echo
+
+
+# Start mysql
+su mysql -c /usr/bin/mysqld_safe &
+sleep 2
+
+# Initialize DB schema
+# TODO: 
+# 
+
+# Start tomcat service
+./bin/start.sh
+
+# Show log files
+echo Waiting for log file...
+while [ ! -f /service/logs/* ]; do
+    sleep 1
+done
+echo /service/logs/*
+tail -F /service/logs/*
