3173910121d0a56a61143c372a55aaef5f597614
Gary Wu <gary.i.wu@huawei.com>
2016-09-22 17:55:32 -0700

    Add mandatory parameters to require MSB_ADDR microservice bus address
    Change-Id: I2f63af321e0ed1c0ee1f8ff8579b723692fa2173
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-tosca-catalog/docker-entrypoint.sh
+++ test/csit/docker/common-tosca-catalog/docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ -z "$MSB_ADDR" ]; then
+    echo "Missing required variable MSB_ADDR: Microservices Service Bus address <ip>:<port>"
+    exit 1
+fi
+
+# Configure MSB IP address
+sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" conf/catalog.yml
+cat conf/catalog.yml
+
+
 # Start mysql
 su mysql -c /usr/bin/mysqld_safe &
 sleep 2
@@ -8,7 +18,7 @@ sleep 2
 /usr/bin/mysqladmin -u root password 'rootpass'
 
 # Initialize DB schema
-/service/initDB.sh root rootpass 3306 127.0.0.1
+./initDB.sh root rootpass 3306 127.0.0.1
 
 # Start service
-/service/run.sh
+./run.sh
