2d27884510e925fcf93ed23f17bd4e276197d233
Gary Wu <gary.i.wu@huawei.com>
2016-09-23 10:00:07 -0700

    Add docker image for external-system-registration
    Change-Id: I12709b9bcc7c62afdd03a839bf107136563485f5
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/common-services-external-system-registration/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$MSB_ADDR" ]; then
+    echo "Missing required variable MSB_ADDR: Microservices Service Bus address <ip>:<port>"
+    exit 1
+fi
+
+# Configure MSB IP address
+sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" conf/extsys.yml
+cat conf/extsys.yml
+
+
+# Start mysql
+su mysql -c /usr/bin/mysqld_safe &
+sleep 2
+
+# Set mysql root password
+/usr/bin/mysqladmin -u root password 'rootpass'
+
+# Initialize DB schema
+./initDB.sh root rootpass 3306 127.0.0.1
+
+# Start service
+./run.sh
