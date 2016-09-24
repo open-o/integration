cd3cafb4ae7abbff4eb7f67d844a7bd1f69d03f6
Gary Wu <gary.i.wu@huawei.com>
2016-09-24 15:18:09 -0700

    Add auto-generation notice header to Dockerfiles
    Change-Id: Ia681853fabec401434c8aa498f793956dbe96857
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/scripts/gen-all-dockerfiles.sh
+++ test/csit/docker/scripts/gen-all-dockerfiles.sh
@@ -6,5 +6,13 @@ ROOT=`git rev-parse --show-toplevel`/test/csit/docker
 cd $ROOT
 for file in `find -name Dockerfile`; do
     dir=`dirname $file`
-    cat $dir/*.txt > $dir/Dockerfile
+    cat > $dir/Dockerfile <<EOF
+#
+# This file was auto-generated; do not modify manually.  Instead, modify the component
+# templates and then run gen-all-dockerfiles.sh to re-generate.
+#
+# $dir Dockerfile
+#
+EOF
+    cat $dir/*.txt >> $dir/Dockerfile
 done
