d44be6d9eeca20b9e8bb6bc5c3fa525dce657bf9
Gary Wu <gary.i.wu@huawei.com>
2016-09-23 23:02:29 -0700

    Increase wait time between docker calls
    Change-Id: I8e9ed3140c128afdd3ce5cdb7e79793df72e96bb
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/scripts/kill-instance.sh
+++ test/csit/docker/scripts/kill-instance.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -v
 # $1 nickname for the instance
 
 docker kill $1
+sleep 1
+
 docker rm $1
+sleep 1
 
--- test/csit/docker/scripts/run-instance.sh
+++ test/csit/docker/scripts/run-instance.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -v
 # $1 docker image path under ${WORKSPACE}/test/csit/docker/
 # $2 nickname for the instance
 # $3 docker run options, e.g. variables
 
 build-image.sh $1
+sleep 3
+
 docker run --name $2 $3 -d $1
+sleep 3
