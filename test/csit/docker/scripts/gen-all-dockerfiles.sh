399ff373fbbc1e39bf1dbbda96da2a48dcd06741
Gary Wu <gary.i.wu@huawei.com>
2016-09-24 14:58:58 -0700

    Refactor Dockerfiles into component templates
    Change-Id: I4a10bac4c6fed8211cdb23931790a8d701b108e6
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/scripts/gen-all-dockerfiles.sh
+++ test/csit/docker/scripts/gen-all-dockerfiles.sh
@@ -5,9 +5,6 @@ ROOT=`git rev-parse --show-toplevel`/test/csit/docker
 
 cd $ROOT
 for file in `find -name Dockerfile`; do
-    dir=$(dirname $file)
-    image=$(basename $dir)
-    echo 
-    echo $image
-    docker build -t $image $dir
+    dir=`dirname $file`
+    cat $dir/*.txt > $dir/Dockerfile
 done
