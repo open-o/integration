d783f4303c8407e32c3e7434af38d0825ee54fb8
Gary Wu <gary.i.wu@huawei.com>
2016-10-06 14:11:55 -0700

    Update docker image for GSO GUI
    Change-Id: I2e79c3e0bb574c9309857ba2a867d9ce16412065
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/gso-service-gateway/instance-config.sh
@@ -0,0 +1,8 @@
+# Initialize DB schema
+MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
+sed -i "s|msb\.openo\.org|${MSB_IP}|" etc/conf/restclient.json
+cat etc/conf/restclient.json
+
+# Set self IP
+sed -i "s|getInputIP|`hostname -i`|" etc/register/service.json
+cat etc/register/service.json
---
+++ test/csit/docker/gso-service-gateway/instance-run.sh
@@ -0,0 +1,11 @@
+# Start tomcat service
+cd bin
+./start.sh
+
+# Show log files
+echo Waiting for log file...
+while [ ! -f /service/logs/* ]; do
+    sleep 1
+done
+echo /service/logs/*
+tail -F /service/logs/*
