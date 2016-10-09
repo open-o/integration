6fab82ca679cc215ce5e0b8611e6ad09a7b2e4ae
Gary Wu <gary.i.wu@huawei.com>
2016-10-09 15:36:38 -0700

    Update docker image for common-services-auth
    Change-Id: I57dd622c7ab7edfe4f46e328063df99d53f3836a
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/common-services-auth/instance-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -v
+
+# Configure keystone
+ADMIN_TOKEN=$(openssl rand -hex 10)
+KEYSTONE_PORT=5000
+
+sed -i "s|IP=.*|IP=127.0.0.1|" webapps/ROOT/WEB-INF/classes/auth_service.properties
+sed -i "s|PORT=.*|PORT=$KEYSTONE_PORT|" webapps/ROOT/WEB-INF/classes/auth_service.properties
+cat webapps/ROOT/WEB-INF/classes/auth_service.properties
+
+sed -i "s|admin_token=.*|admin_token=$ADMIN_TOKEN|" webapps/ROOT/WEB-INF/classes/keystone_config.properties
+cat webapps/ROOT/WEB-INF/classes/keystone_config.properties
+
+sed -i "s|#admin_token.*|admin_token=$ADMIN_TOKEN|" /etc/keystone/keystone.conf
+sed -i "s|#public_port.*|public_port=$KEYSTONE_PORT|" /etc/keystone/keystone.conf
+cat /etc/keystone/keystone.conf | egrep -v '^[[:space:]]*$|^ *#'
+
--- test/csit/docker/common-services-auth/instance-run.sh
+++ test/csit/docker/common-services-auth/instance-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -v
 
+# Start keystone
+keystone-all -d -v &
+
 # Start tomcat service
 ./bin/start.sh
 
 # Show log files
 echo Waiting for log file...
 while [ ! -f /service/logs/* ]; do
+    ps -ef
     sleep 1
 done
 echo /service/logs/*
