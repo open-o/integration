2e313194b40982b7ae9f98aad8aa629659e57736
Gary Wu <gary.i.wu@huawei.com>
2016-10-09 16:35:26 -0700

    Set up docker image for sdno-driver-huawei-openstack
    Change-Id: Ib771249eb7fcccc16c00958e75fe071aa307d65d
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/sdno-driver-huawei-openstack/instance-workaround.sh
+++ test/csit/docker/sdno-driver-huawei-openstack/instance-workaround.sh
@@ -1,6 +1,6 @@
-# fix path issue to catalina.sh in bin/start.sh
-sed -i 's|catalina.sh|$CATALINA_HOME/bin/catalina.sh|' ./bin/{start.sh,stop.sh}
-
 # proxy connection to MSB
 socat TCP-LISTEN:8080,fork TCP:$MSB_ADDR &
 
+# fix broken start/stop scripts
+sed -i '1i#!/bin/bash'
+dos2unix bin/{start.sh,stop.sh}
