4362cc4c37f71827646e58f392f506005e6424f1
Gary Wu <gary.i.wu@huawei.com>
2016-10-12 21:03:57 -0700

    Update docker image for common-services-wso2ext
    Change-Id: If7b2f77df992bf182ebf62eaf8d09a6f3a3db0d6
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 0acf65ec73a36eef8d14328f62ffe48d5713a976)

--- test/csit/docker/common-services-wso2ext/instance-run.sh
+++ test/csit/docker/common-services-wso2ext/instance-run.sh
@@ -1,6 +1,8 @@
 # Start wso2bps
-./wso2bps-3.6.0/bin/wso2server.sh --start
+./wso2bps/bin/wso2server.sh --start
 sleep 3
 
 # Start service
 ./run.sh
+sleep 1
+tail -F wso2bps-ext/logs/wso2bpel-ext.log
