feecf32fc329b9e7fdd33abf4fafa09263978d5a
Chen Cang <cang.chen@huawei.com>
2016-10-14 23:18:38 +0800

    docker workaround for sdn-o services without mysql
    Resolves: SDNO-593
    
    Change-Id: I5e1fe4a9e5399563a77bf1028413b409ccd8765f
    Signed-off-by: Chen Cang <cang.chen@huawei.com>

--- test/csit/docker/sdno-service-vxlan/instance-run.sh
+++ test/csit/docker/sdno-service-vxlan/instance-run.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+# proxy connection to MSB
+nohup socat TCP-LISTEN:8080,fork TCP:$MSB_ADDR  </dev/null >/dev/null 2>&1 &
+
 # Start microservice
 cd bin
 ./start.sh
-while [ ! -e ../logs/catalina.out ]; do
+
+# tail -F not work on remote-fs(BTRFS/AUFS)
+while [ ! -e /service/logs/application.log ]; do
     sleep 1
 done
-tail -F ../logs/catalina.out
+# keep shell running to prevent container from exit
+tail -f /service/logs/application.log
