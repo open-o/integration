22b73202691cfb86a699216ad65e9bd549d5e050
Gary Wu <gary.i.wu@huawei.com>
2016-10-14 16:54:00 -0700

    Update docker image for nfvo-driver-vnfm-huawei
    Change-Id: I137ed89c6fd4ae54592971d1ccd178ed302094b0
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/nfvo-driver-vnfm-huawei/instance-init.sh
+++ test/csit/docker/nfvo-driver-vnfm-huawei/instance-init.sh
@@ -1,5 +1,5 @@
-# Set mysql root password
-sed -i 's|Test_12345|rootpass|' ./webapps/vnfmadapter-service/WEB-INF/classes/spring/Vnfmadapter/services.xml
+# Config mysql credentials
+sed -i "s|Test_12345|rootpass|" webapps/ROOT/WEB-INF/classes/spring/Vnfmadapter/services.xml
 
 # Initialize MySQL schema
 cd bin
