8cbb010cf9c8f768331bcead89b7d38654f3baf5
Gary Wu <gary.i.wu@huawei.com>
2016-10-14 17:10:23 -0700

    Update docker image for nfvo-resmanagement
    Change-Id: I7763ce5ddb3645c3e16a1e1f0c2aab08e2a4d1ff
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 1af7ff1cb2d0b1795b3524dc01a948b278f9fe7f)

--- test/csit/docker/nfvo-resmanagement/instance-init.sh
+++ test/csit/docker/nfvo-resmanagement/instance-init.sh
@@ -1,5 +1,5 @@
 # Set mysql root password
-sed -i 's|Test_12345|rootpass|' ./webapps/resmanagement-service/WEB-INF/classes/spring/Resmanagement/services.xml
+sed -i 's|123456|rootpass|' ./webapps/ROOT/WEB-INF/classes/spring/Resmanagement/services.xml
 
 # Initialize MySQL schema
 cd bin
