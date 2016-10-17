33cbb4e4e48cadac38a6b26f09f25c6615cdae8d
Gary Wu <gary.i.wu@huawei.com>
2016-10-16 21:25:37 -0700

    Add script to pull all Open-O docker images
    Change-Id: Id4a05a6b230980d383cfa073cd11931200d000aa
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/scripts/pull-all-images.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -exu
+
+VERSION="1.0.0-RC0"
+
+# docker root dir
+ROOT=`git rev-parse --show-toplevel`/test/csit/docker
+
+cd $ROOT
+for file in `find -name Dockerfile`; do
+    dir=$(dirname $file)
+    image=$(basename $dir)
+    echo 
+    echo $image
+    docker pull openoint/$image:$VERSION
+    docker pull openoint/$image:latest
+done
