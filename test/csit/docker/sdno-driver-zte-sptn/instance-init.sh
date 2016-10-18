37aac5ad95aabac903944c093b7a73833013702d
Gary Wu <gary.i.wu@huawei.com>
2016-10-18 16:23:05 -0700

    Update docker image for sdno-driver-zte-sptn
    Change-Id: I9710258105db91b3f5b213a105f0e6df6325be7d
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 21e050ef71688cc916475096ea1f813a97ac286a)

--- test/csit/docker/sdno-driver-zte-sptn/instance-init.sh
+++ test/csit/docker/sdno-driver-zte-sptn/instance-init.sh
@@ -1,2 +1,8 @@
 # Initialize DB schema
 ./initDB.sh root rootpass 3306 127.0.0.1
+
+# Set mysql username/password
+sed -i "s|user:.*|user: root|" conf/config.yaml
+sed -i "s|password:.*|password: rootpass|" conf/config.yaml
+sed -i "s|url:.*|url: jdbc:mysql://localhost/inventory|" conf/config.yaml
+cat conf/config.yaml
