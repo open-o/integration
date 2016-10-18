a827b08e4c619cddb551e6ebc90b82df143a4e84
Gary Wu <gary.i.wu@huawei.com>
2016-10-18 14:43:07 -0700

    Add Kubernetes node setup script for CentOS 7
    Change-Id: I64a7100fa3e3fee60129a84d154619764ecbd235
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/kubernetes/setup-centos7.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Run as root
+if [ $USER != "root" ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+
+curl "https://gerrit.open-o.org/r/gitweb?p=integration.git;a=blob_plain;f=test/csit/vagrant/weave.service;hb=refs/heads/master" > /etc/systemd/system/weave.service
+curl "https://gerrit.open-o.org/r/gitweb?p=integration.git;a=blob_plain;f=test/csit/vagrant/kubernetes.repo;hb=refs/heads/master" > /etc/yum.repos.d/kubernetes.repo
+
+yum -y update
+yum -y install docker kubelet kubeadm kubectl kubernetes-cni
+systemctl enable docker && systemctl start docker
+curl -L git.io/weave -o /usr/local/bin/weave
+mkdir -p /opt/cni/bin
+mkdir -p /etc/cni/net.d
+systemctl enable weave
+systemctl enable kubelet && systemctl start kubelet
