f66ae3ab87eecb1e8bf75c9ee622f145913b13a1
fujinhua <fu.jinhua@zte.com.cn>
2016-10-19 10:45:57 +0800

    INT-63 fix msb-port config of nfvo-lcm
    Change-Id: Id94978a15f161a5cf251a6e9f7b3a7b3172d7a63
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- test/csit/docker/nfvo-lcm/instance-config.sh
+++ test/csit/docker/nfvo-lcm/instance-config.sh
@@ -1,5 +1,7 @@
 # Configure MSB IP address
 MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
+MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" nfvo/lcm/lcm/pub/config/config.py
+sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|DB_NAME.*|DB_NAME = 'inventory'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|DB_PASSWD.*|DB_PASSWD = 'rootpass'|" nfvo/lcm/lcm/pub/config/config.py
