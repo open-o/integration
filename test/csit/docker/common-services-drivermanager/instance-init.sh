ca2b229f24709ec027c1e1c95e4ca842deedfcc5
Gary Wu <gary.i.wu@huawei.com>
2016-10-19 21:41:59 -0700

    Add missing DB password configuration for common-services-drivermanager
    Change-Id: I5d07f1af8b1d90bd5ab538e09790e7617cdd3750
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-services-drivermanager/instance-init.sh
+++ test/csit/docker/common-services-drivermanager/instance-init.sh
@@ -2,3 +2,6 @@
 
 # Initialize DB schema
 mysql -uroot -prootpass < init/drivermanagerservice_tables_mysql.sql
+
+# Set mysql password
+sed -i 's|Changeme_123|rootpass|' webapps/ROOT/WEB-INF/classes/mybatis/configuration/configuration.xml
