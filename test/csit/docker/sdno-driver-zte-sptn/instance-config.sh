ea1b2a54bed94bdf7a77f050df123bd536170768
Gary Wu <gary.i.wu@huawei.com>
2016-10-20 14:31:05 -0700

    Updated docker image for sdno-driver-zte-sptn
    Change-Id: I92db1966683c3aa2ae0c29cd65db01ab531d3a92
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit c3a6156b90f04a906ab8e291decf4faffc834c5a)

--- test/csit/docker/sdno-driver-zte-sptn/instance-config.sh
+++ test/csit/docker/sdno-driver-zte-sptn/instance-config.sh
@@ -1,3 +1,6 @@
 # Configure MSB IP address
 sed -i "s|msbUrl:.*|msbUrl: http://$MSB_ADDR|" conf/config.yaml
 cat conf/config.yaml
+
+sed -i "s|127\.0\.0\.1|$SERVICE_IP|" conf/driver.json
+cat conf/driver.json
--- test/csit/docker/sdno-driver-zte-sptn/instance-init.sh
+++ test/csit/docker/sdno-driver-zte-sptn/instance-init.sh
@@ -4,5 +4,4 @@
 # Set mysql username/password
 sed -i "s|user:.*|user: root|" conf/config.yaml
 sed -i "s|password:.*|password: rootpass|" conf/config.yaml
-sed -i "s|url:.*|url: jdbc:mysql://localhost/inventory|" conf/config.yaml
 cat conf/config.yaml
