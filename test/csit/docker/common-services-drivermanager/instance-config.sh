90e0d904703b981c68e06a4b6047a111631e752b
Gary Wu <gary.i.wu@huawei.com>
2016-10-20 17:42:15 -0700

    Update docker image for common-services-drivermanager
    Change-Id: Ie60abffa152a5a958c4f7ad35fd623689cfcc04e
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-services-drivermanager/instance-config.sh
+++ test/csit/docker/common-services-drivermanager/instance-config.sh
@@ -9,3 +9,9 @@ echo
 sed -i "s|10\.229\.47\.199|$SERVICE_IP|" etc/microservice/driver_rest.json
 cat etc/microservice/driver_rest.json
 echo
+
+MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
+sed -i "s|IP=.*|IP=$MSB_IP|" webapps/ROOT/WEB-INF/classes/driver_manager.properties
+cat webapps/ROOT/WEB-INF/classes/driver_manager.properties
+echo
+
