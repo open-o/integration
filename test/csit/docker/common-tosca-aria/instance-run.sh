93e8f786a1449e11e2649467429454e40e2fc33a
Gary Wu <gary.i.wu@huawei.com>
2016-10-26 16:14:26 -0700

    Update docker image for common-tosca-aria
    Change-Id: I50e8770c9f4520b4354a0ea8958ac53dbd4c4d75
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit d6280056464bdaed06f2cc9a72663a0aebec2ff1)

--- test/csit/docker/common-tosca-aria/instance-run.sh
+++ test/csit/docker/common-tosca-aria/instance-run.sh
@@ -1,2 +1,4 @@
+# Run microservice
 source env/bin/activate
-open-o-common-tosca-parser-service start
+open-o-common-tosca-parser-service start --rundir log
+tail -F log/open-o-common-tosca-parser-service.log
