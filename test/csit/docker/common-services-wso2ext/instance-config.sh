60b739291d8e44e81fd4aebad20c979a6aeb07d6
Gary Wu <gary.i.wu@huawei.com>
2016-10-31 07:16:05 -0700

    Update docker image script for common-services-wso2ext
    Change-Id: I8b27ef021040c3206b586ee2c419ff4602ec4e9d
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-services-wso2ext/instance-config.sh
+++ test/csit/docker/common-services-wso2ext/instance-config.sh
@@ -1,3 +1,7 @@
 # Configure MSB IP address
 sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" wso2bps-ext/conf/wso2bpel.yml
+sed -i "s|serviceIp:.*|serviceIp: $SERVICE_IP|" wso2bps-ext/conf/wso2bpel.yml
 cat wso2bps-ext/conf/wso2bpel.yml
+
+sed -i "s|MSB_URL=.*|MSB_URL=http://$MSB_ADDR|" wso2bps/wso2bps-ext.properties
+cat wso2bps/wso2bps-ext.properties
