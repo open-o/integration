b8ed11737002976777c97fe5ea288c74c237801f
Gary Wu <gary.i.wu@huawei.com>
2016-10-30 21:51:58 -0700

    Add $SERVICE_IP support to ESR, catalog, and inventory
    Change-Id: I6443c4c880a3a1d3da211a5d024b772638df5065
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 343f772922f14fe247884f629db50ee9457d1c40)

--- test/csit/docker/common-services-extsys/instance-config.sh
+++ test/csit/docker/common-services-extsys/instance-config.sh
@@ -1,3 +1,4 @@
 # Configure MSB IP address
 sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" conf/extsys.yml
+sed -i "s|serviceIp:.*|serviceIp: $SERVICE_IP|" conf/extsys.yml
 cat conf/extsys.yml
--- test/csit/docker/common-tosca-catalog/instance-config.sh
+++ test/csit/docker/common-tosca-catalog/instance-config.sh
@@ -1,3 +1,4 @@
 # Configure MSB IP address
 sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" catalog/conf/catalog.yml
+sed -i "s|serviceIp:.*|serviceIp: $SERVICE_IP|" catalog/conf/catalog.yml
 cat catalog/conf/catalog.yml
