f6f7c6d51b950fcdf6af3845554519cb2de0736a
Gary Wu <gary.i.wu@huawei.com>
2016-10-30 17:42:23 -0700

    Update docker image for common-services-auth; bump build version
    Change-Id: I83fad67c8b8821f93e31e217f374572394a21ec4
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-services-auth/instance-init.sh
+++ test/csit/docker/common-services-auth/instance-init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -v
 
+# Initialize DB schema
+mysql -uroot -prootpass < init/keystone_tables_mysql.sql
+
 # Configure keystone
-ADMIN_TOKEN=$(openssl rand -hex 10)
+openssl rand -hex 10 > admin_token.txt
+
+ADMIN_TOKEN=`cat admin_token.txt`
 KEYSTONE_PORT=5000
 
 sed -i "s|IP=.*|IP=127.0.0.1|" webapps/ROOT/WEB-INF/classes/auth_service.properties
@@ -14,4 +19,19 @@ cat webapps/ROOT/WEB-INF/classes/keystone_config.properties
 sed -i "s|#admin_token.*|admin_token=$ADMIN_TOKEN|" /etc/keystone/keystone.conf
 sed -i "s|#public_port.*|public_port=$KEYSTONE_PORT|" /etc/keystone/keystone.conf
 cat /etc/keystone/keystone.conf | egrep -v '^[[:space:]]*$|^ *#'
+
+sed -i "/^\[DEFAULT\]$/aadmin_token = $ADMIN_TOKEN" /etc/keystone/keystone.conf
+sed -i '/^\[DEFAULT\]$/averbose = True' /etc/keystone/keystone.conf
+sed -i '/^\[database\]$/aconnection = mysql://keystone:root@127.0.0.1/keystone' /etc/keystone/keystone.conf
+sed -i '/^\[memcache\]$/aservers = localhost:11211' /etc/keystone/keystone.conf
+sed -i '/^\[token\]$/aprovider = uuid' /etc/keystone/keystone.conf
+sed -i '/^\[token\]$/adriver = memcache' /etc/keystone/keystone.conf
+sed -i '/^\[revoke\]$/adriver = sql' /etc/keystone/keystone.conf
+
+#config httpd and run it
+sed -i '/\#ServerName www.example.com:80$/aServerName controller' /etc/httpd/conf/httpd.conf
+cp init/wsgi-keystone.conf  /etc/httpd/conf.d/wsgi-keystone.conf
+
+#Populate the Identity service database
+su -s /bin/sh -c "keystone-manage db_sync" keystone
 
--- test/csit/docker/common-services-auth/instance-run.sh
+++ test/csit/docker/common-services-auth/instance-run.sh
@@ -1,10 +1,35 @@
 #!/bin/bash -v
 
+# run httpd
+/usr/sbin/httpd
+
 # Start keystone
 keystone-all -d -v &
+sleep 1
+
+
+unset http_proxy
+export OS_TOKEN=`cat admin_token.txt`
+export OS_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+openstack service create  --name keystone --description "OpenStack Identity" identity
+openstack endpoint create --region RegionOne   identity public http://controller:5000/v2.0
+openstack endpoint create --region RegionOne   identity internal http://controller:5000/v2.0
+openstack endpoint create --region RegionOne   identity admin http://controller:35357/v2.0
+
+#Create projects, users, and roles
+openstack project create --domain default   --description "Admin Project" admin
+openstack user create --domain default   --password  Changeme_123 admin
+openstack role create admin
+openstack role add --project admin --user admin admin
+
+#start memcached
+/usr/bin/memcached -u root &
+
 
 # Start tomcat service
 ./bin/start.sh
+
 
 # Show log files
 echo Waiting for log file...
