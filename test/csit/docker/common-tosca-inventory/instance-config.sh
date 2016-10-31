343f772922f14fe247884f629db50ee9457d1c40
Gary Wu <gary.i.wu@huawei.com>
2016-10-30 21:51:58 -0700

    Add $SERVICE_IP support to ESR, catalog, and inventory
    Change-Id: I6443c4c880a3a1d3da211a5d024b772638df5065
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/common-tosca-inventory/instance-config.sh
+++ test/csit/docker/common-tosca-inventory/instance-config.sh
@@ -1,3 +1,4 @@
 # Configure MSB IP address
 sed -i "s|msbServerAddr:.*|msbServerAddr: http://$MSB_ADDR|" conf/inventory.yml
+sed -i "s|serviceIp:.*|serviceIp: $SERVICE_IP|" conf/inventory.yml
 cat conf/inventory.yml
