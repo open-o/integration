fc0a318044e35c84acc654fddc58b9d50f945871
Gary Wu <gary.i.wu@huawei.com>
2016-11-02 13:02:47 -0700

    Add support for MYSQL_ADDR env variable to gso-service-manager image
    Change-Id: Ibf8841d369ea8f474920c5472a4b5f4e48a61b7e
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit e96398a13c1b4c738d30dd246959c1a0d756950a)

--- test/csit/docker/gso-service-manager/instance-config.sh
+++ test/csit/docker/gso-service-manager/instance-config.sh
@@ -6,3 +6,16 @@ cat etc/conf/restclient.json
 # Set self IP
 sed -i "s|getInputIP|$SERVICE_IP|" etc/register/service.json
 cat etc/register/service.json
+
+# Configure MYSQL
+if [ -z "$MYSQL_ADDR" ]; then
+    export MYSQL_IP=`hostname -i`
+    export MYSQL_PORT=3306
+    export MYSQL_ADDR=$MYSQL_IP:$MYSQL_PORT
+else
+    MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
+    MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
+fi
+echo "MYSQL_ADDR=$MYSQL_ADDR"
+sed -i "s|jdbc:mysql://[^/]*/|jdbc:mysql://$MYSQL_ADDR/|" webapps/ROOT/WEB-INF/classes/spring/service.xml
+cat webapps/ROOT/WEB-INF/classes/spring/service.xml
