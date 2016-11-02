37a114673596eb9175996704e6c9a89d5a10d149
Gary Wu <gary.i.wu@huawei.com>
2016-11-02 12:52:51 -0700

    Add support for MYSQL_ADDR env variable to nfvo-lcm image
    Change-Id: I7cc743789ba36b75bbb6fd489c26418d123e35d0
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/nfvo-lcm/instance-config.sh
+++ test/csit/docker/nfvo-lcm/instance-config.sh
@@ -6,6 +6,20 @@ sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" nfvo/lcm/lcm/pub/c
 sed -i "s|DB_NAME.*|DB_NAME = 'inventory'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|DB_PASSWD.*|DB_PASSWD = 'rootpass'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" nfvo/lcm/lcm/pub/config/config.py
+
+# Configure MYSQL
+if [ -z "$MYSQL_ADDR" ]; then
+    export MYSQL_IP=`hostname -i`
+    export MYSQL_PORT=3306
+    export MYSQL_ADDR=$MYSQL_IP:$MYSQL_PORT
+else
+    MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
+    MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
+fi
+echo "MYSQL_ADDR=$MYSQL_ADDR"
+sed -i "s|DB_IP.*|DB_IP = '$MYSQL_IP'|" nfvo/lcm/lcm/pub/config/config.py
+sed -i "s|DB_PORT.*|DB_PORT = $MYSQL_PORT|" nfvo/lcm/lcm/pub/config/config.py
+
 cat nfvo/lcm/lcm/pub/config/config.py
 
 sed -i "s|127\.0\.0\.1|$SERVICE_IP|" nfvo/lcm/run.sh
