32c7b7e0de9194ca6bccc0e0745d9262b3e76cea
Gary Wu <gary.i.wu@huawei.com>
2016-11-03 10:53:17 -0700

    Update docker image for nfvo-lcm
    Change-Id: I572ecd78906cb5c6b78a94ed963d5ef1b2793dd0
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/docker/nfvo-lcm/instance-config.sh
+++ test/csit/docker/nfvo-lcm/instance-config.sh
@@ -4,7 +4,8 @@ MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|DB_NAME.*|DB_NAME = 'inventory'|" nfvo/lcm/lcm/pub/config/config.py
-sed -i "s|DB_PASSWD.*|DB_PASSWD = 'rootpass'|" nfvo/lcm/lcm/pub/config/config.py
+sed -i "s|DB_USER.*|DB_USER = 'inventory'|" nfvo/lcm/lcm/pub/config/config.py
+sed -i "s|DB_PASSWD.*|DB_PASSWD = 'inventory'|" nfvo/lcm/lcm/pub/config/config.py
 sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" nfvo/lcm/lcm/pub/config/config.py
 
 # Configure MYSQL
