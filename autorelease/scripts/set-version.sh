fe89772c39c47cf3f2218788b88596dbc079403e
Gary Wu <gary.i.wu@huawei.com>
2016-11-03 12:15:01 -0700

    Update version to 1.0.0
    Change-Id: I663b4fb995cd93ebd4c9d90c4051513b2d0f7226
    Issue-id: INT-32
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- autorelease/scripts/set-version.sh
+++ autorelease/scripts/set-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="1.0.0-RC1"
+VERSION="1.0.0"
 
 # autorelease root dir
 ROOT=`git rev-parse --show-toplevel`/autorelease
@@ -9,6 +9,16 @@ BUILD_DIR=$ROOT/build
 
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
+
+# reset wayward SNAPSHOT versions first
+perl -p -i -e 's/\$\{brs\.version\}/1.0.0-SNAPSHOT/g' `find . -name pom.xml`
+
+# workaround for master branch version merged into sun
+perl -p -i -e 's/1\.1\.0+-SNAPSHOT/1.0.0-SNAPSHOT/g' `find . -name pom.xml`
+
+if [ -z "$MVN" ]; then
+    export MVN=`which mvn`
+fi
 
 # Jenkins sets a $MVN parameter via JJB that points to the appropriately installed maven
 $MVN -f oparent/version/pom.xml versions:set versions:update-child-modules -DnewVersion=${VERSION}
