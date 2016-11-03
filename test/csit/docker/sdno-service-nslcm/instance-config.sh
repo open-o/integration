6743ed23c809f1bf9e9365924b9d68e33ae40103
Gary Wu <gary.i.wu@huawei.com>
2016-11-03 11:00:36 -0700

    Update docker image for sdno-service-nslcm
    Change-Id: I8cf7c26f7812db5c481c76af0c4910445803da85
    Issue-id: INT-18
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 79b38da01f11fdf967cb36c9d859dfe6942d2ef5)

--- test/csit/docker/sdno-service-nslcm/instance-config.sh
+++ test/csit/docker/sdno-service-nslcm/instance-config.sh
@@ -10,3 +10,9 @@ fi
 echo "MYSQL_ADDR=$MYSQL_ADDR"
 sed -i "s|\${jdbc\.host}\:\${jdbc.port}|$MYSQL_ADDR|" webapps/ROOT/WEB-INF/classes/spring/service.xml
 cat webapps/ROOT/WEB-INF/classes/spring/service.xml
+
+sed -i "s|jdbc.host=.*|jdbc.host=$MYSQL_IP|" webapps/ROOT/WEB-INF/classes/jdbc.properties
+sed -i "s|jdbc.port=.*|jdbc.port=$MYSQL_PORT|" webapps/ROOT/WEB-INF/classes/jdbc.properties
+sed -i "s|jdbc.username=.*|jdbc.username=inventory|" webapps/ROOT/WEB-INF/classes/jdbc.properties
+sed -i "s|jdbc.password=.*|jdbc.password=inventory|" webapps/ROOT/WEB-INF/classes/jdbc.properties
+cat webapps/ROOT/WEB-INF/classes/jdbc.properties
