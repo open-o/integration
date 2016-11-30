40917d0c41c24dd1d52cf2d7bbc3e4a6c1d46c15
Viorel Mirea <viorel.mirea@huawei.com>
2016-11-30 00:46:39 +0000

    SDNO-404 + SDNO-410 removed duplcate code
    Change-Id: I965026ac42e35f13c865dc2fa09f241e166e92e4
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

--- test/csit/plans/sdno-l3vpn/sanity-check/setup.sh
+++ test/csit/plans/sdno-l3vpn/sanity-check/setup.sh
@@ -1,20 +1,19 @@
 # These scripts are sourced by run-csit.sh.
+source ${SCRIPTS}/common_functions.sh
 
 # Start MSB
 ${SCRIPTS}/common-services-microservice-bus/startup.sh i-msb
 MSB_IP=`get-instance-ip.sh i-msb`
-for i in {1..10}; do
-    curl -sS http://${MSB_IP}/openoui/microservices/index.html | grep "org_openo_msb_route_title" && break
-    echo "MSB wait" sleep $i
-    sleep $i
-done
+sleep_msg="Waiting_for_MSB_load"
+curl_path='http://'${MSB_IP}'/openoui/microservices/index.html'
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE="$sleep_msg" REPEAT_NUMBER=10 GREP_STRING="org_openo_msb_route_title"
 
 # Start BRS
 ${SCRIPTS}/sdno-brs/startup.sh i-brs ${MSB_IP}:80
 BRS_IP=`get-instance-ip.sh i-brs`
 
-${SCRIPTS}/sdno-l3vpn/startup.sh i-l3vpn ${MSB_IP}:80
-BRS_IP=`get-instance-ip.sh i-l3vpn`
+${SCRIPTS}/sdno-l3vpn/startup.sh s-l3vpn ${MSB_IP}:80
+BRS_IP=`get-instance-ip.sh s-l3vpn`
 
 SERVICE_PORT='8506'
 SERVICE_NAME='sdnol3vpn'
--- test/csit/scripts/sdno-overlay/startup.sh
+++ test/csit/scripts/sdno-overlay/startup.sh
@@ -5,7 +5,7 @@ source ${SCRIPTS}/common_functions.sh
 echo $@
 
 #Start OVERLAY
-run-instance.sh openoint/sdno-service-overlay $1 " -i -t -e MSB_ADDR=$2"
+run-instance.sh openoint/sdno-service-overlayvpn $1 " -i -t -e MSB_ADDR=$2"
 curl_path='http://'$2'/openoapi/microservices/v1/services/sdnooverlay/version/v1'
 sleep_msg="Waiting_connection_of_url_for:"$1
 wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="status"
