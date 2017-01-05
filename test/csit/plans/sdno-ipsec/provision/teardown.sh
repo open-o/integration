40c6472e9e635c7b83eb3aed03751d6a20b5cefb
Viorel Mirea <viorel.mirea@huawei.com>
2017-01-05 23:39:54 +0000

    Added Apache License and fixed setup.sh
    Issue-id: SDNO-352
    
    Change-Id: I8f2ed238964e12181040a8cd34b2f99a2be21192
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

--- test/csit/plans/sdno-ipsec/provision/teardown.sh
+++ test/csit/plans/sdno-ipsec/provision/teardown.sh
@@ -1,11 +1,26 @@
 # This script is sourced by run-csit.sh after Robot test completion.
 
-kill-instance.sh d-driver-huawei-openstack
-kill-instance.sh d-driver-huawei-overlay
-kill-instance.sh s-ipsec
-kill-instance.sh i-common-services-extsys
-kill-instance.sh d-drivermgr
-kill-instance.sh i-brs
-kill-instance.sh i-mss
-kill-instance.sh i-msb
-kill  `ps -ax | grep java| grep moco | awk '{print $1}'`
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+kill-instance.sh d-driver-huawei-openstack || true
+kill-instance.sh d-driver-huawei-overlay || true
+kill-instance.sh s-ipsec || true
+kill-instance.sh i-common-services-extsys || true
+kill-instance.sh d-drivermgr || true
+kill-instance.sh i-brs || true
+kill-instance.sh i-mss || true
+kill-instance.sh i-msb || true
+kill  `ps -ax | grep java| grep moco | awk '{print $1}'` || true
