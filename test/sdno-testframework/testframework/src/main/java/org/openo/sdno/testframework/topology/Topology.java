24d511f794b77aec62bdcdeab4bee02ec7cb3d0d
Murali-P <murali.p@huawei.com>
2017-01-11 16:31:29 +0530

    Enhance create/clear topology
    Resolved:INT-77
    Robot wrapper over Test Framework to generate useful keywords
    
    Change-Id: If2ad30a099211a712aacf0d856e67c1291236f39
    Signed-off-by: Murali-P <murali.p@huawei.com>

--- test/sdno-testframework/testframework/src/main/java/org/openo/sdno/testframework/topology/Topology.java
+++ test/sdno-testframework/testframework/src/main/java/org/openo/sdno/testframework/topology/Topology.java
@@ -17,14 +17,30 @@
 package org.openo.sdno.testframework.topology;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.openo.sdno.testframework.checker.JsonSchemaValidator;
+import org.openo.sdno.testframework.checker.ServiceExceptionChecker;
+import org.openo.sdno.testframework.http.model.HttpModelUtils;
+import org.openo.sdno.testframework.http.model.HttpRequest;
+import org.openo.sdno.testframework.http.model.HttpResponse;
+import org.openo.sdno.testframework.http.model.HttpRquestResponse;
+import org.openo.sdno.testframework.replace.PathReplace;
+import org.openo.sdno.testframework.testmanager.TestManager;
 import org.openo.sdno.testframework.util.file.FileUtils;
 import org.openo.sdno.testframework.util.file.JsonFileNameFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONString;
 
 /**
  * Topology Operation Class.<br>
@@ -32,13 +48,29 @@ import org.slf4j.LoggerFactory;
  * @author
  * @version SDNO 0.5 Jun 17, 2016
  */
-public class Topology {
+public class Topology extends TestManager{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Topology.class);
 
     private String topoFileDir;
+    
+    private String resourceId;
+    
+    private File file;
+    
+    private Map<String, String> mapOfElements; 
 
     private TopologyOperation topoOperation;
+    
+    Map<String, Map<String, String>> mapOfTopoElements = new HashMap<String, Map<String, String>> ();
+    
+    private static final Map<String, String> TypeMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("NEelement", "id");
+        aMap.put("Controller", "controllerId");
+        TypeMap = Collections.unmodifiableMap(aMap);
+    }
 
     /**
      * Constructor<br>
@@ -79,9 +111,48 @@ public class Topology {
             throw new ServiceException("topoFileDir is invalid!!");
         }
 
-        List<File> resFileList = FileUtils.listAllFiles(new File(this.topoFileDir), new JsonFileNameFilter());
-        topoOperation.addInvResourceData(resFileList);
+        List<File> resFileList = FileUtils.listAllFiles(new File(this.topoFileDir), new JsonFileNameFilter());   
+        
+        Map<String, String> mapOfElements =  new  HashMap<String, String>();
+        for (File file: resFileList)
+        {
+            String path = file.getParent(); 
+            String directoryName =path.substring(path.lastIndexOf("\\")+1,path.length()); 
+            resourceId = doOperationAndGetValue (file.getAbsolutePath(), TypeMap.get(directoryName)); 
+            mapOfElements.put(file.getName(), resourceId);
+        }
+        
+        mapOfTopoElements.put(topoFileDir, mapOfElements);
     }
+    
+  
+    
+    public String doOperationAndGetValue(String queryPath, String variable) throws ServiceException {
+        HttpRquestResponse httpCreateObject = HttpModelUtils.praseHttpRquestResponseFromFile(queryPath);
+        HttpRequest createRequest = httpCreateObject.getRequest();
+        HttpResponse createResponse =
+                execTestCase(createRequest, new ServiceExceptionChecker());
+        String  resourceId = getObject(variable, createResponse.getData());
+       
+        
+        return resourceId;
+
+    }
+    
+    public String getObject(String key, String JSON) {
+        Pattern p = Pattern.compile(String.format("\"%s\":\"(.*?)\"", key));
+        Matcher matcher = p.matcher(JSON);
+        if(matcher.find())
+        {
+           Pattern resource = Pattern.compile(String.format(":\"(.*?)\"", key));
+           Matcher match = resource.matcher(matcher.group());
+           if(match.find())
+           {
+               return match.group().substring(1).replaceAll("\"", "");
+           }
+        }
+        return null;
+     }
 
     /**
      * Clear All Topology Resource Data.<br>
@@ -90,6 +161,7 @@ public class Topology {
      * @since SDNO 0.5
      */
     public void clearTopology() throws ServiceException {
+        
         topoOperation.removeResourceData();
     }
 
@@ -100,7 +172,44 @@ public class Topology {
      * @since SDNO 0.5
      */
     public void clearInvTopology() throws ServiceException {
-        topoOperation.removeInvResourceData();
+        
+        if(StringUtils.isBlank(topoFileDir)) {
+            LOGGER.error("topoFile Dir parameter is invalid!!");
+            throw new ServiceException("topoFileDir is invalid!!");
+        }
+
+        List<File> resFileList = FileUtils.listAllFiles(new File(this.topoFileDir), new JsonFileNameFilter());   
+             
+        Map<String, String> FileNameToId = mapOfTopoElements.get(topoFileDir);
+        if (null == FileNameToId)
+        {
+            // log error
+            return;
+        }
+        
+        // Loop all the files, and get corresponding resource Id and send delete request
+        for (File resFile:resFileList)
+        {
+            String resourceId = FileNameToId.get(resFile.getName());
+            
+            // We have to append URI with Id and change the method to Delete and send.
+            HttpRquestResponse httpDelObject = HttpModelUtils.praseHttpRquestResponseFromFile(resFile.getAbsolutePath());
+            HttpRequest httpRequest = httpDelObject.getRequest();
+            
+            // Apppend the resource Id for delete request to delete the resource   
+            httpRequest.setMethod("delete");
+            httpRequest.setUri(httpRequest.getUri() + "/" + resourceId);
+            
+            // Make data has empty
+            httpRequest.setData("");
+            
+            // Send the delete request and verify the response (only error code);
+            httpDelObject.getResponse().setStatus(200);
+            httpDelObject.getResponse().setData("");
+            HttpResponse delResponse =
+                    execTestCase(httpRequest, new ServiceExceptionChecker());
+        }        
+        
     }
 
     /**
