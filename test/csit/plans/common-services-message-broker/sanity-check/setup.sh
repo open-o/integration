4eeb762add4424606c7d62393018452ac0fb09e5
HuabingZhao <zhao.huabing@zte.com.cn>
2017-01-24 12:59:42 +0800

    Add message broker CSIT test
    Add CSIT test to test the Active MQ listening port
    
    Issue-Id: OCS-199
    Change-Id: I442b5277411246ca69cf01b7c758422d1dcfacf2
    Signed-off-by: HuabingZhao <zhao.huabing@zte.com.cn>
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/plans/common-services-message-broker/sanity-check/setup.sh
+++ test/csit/plans/common-services-message-broker/sanity-check/setup.sh
@@ -22,12 +22,21 @@ MSB_IP=`get-instance-ip.sh i-msb`
 echo MSB_IP=${MSB_IP}
 sleep 5
 
+# Start Message Broker
+${SCRIPTS}/common-services-message-broker/startup.sh i-activemq 
+ACTIVEMQ_IP=`get-instance-ip.sh i-activemq`
+echo ACTIVEMQ_IP=${ACTIVEMQ_IP}
+
+
 # Wait for initialization(8086 Service Registration & Discovery, 80 api gateway)
 for i in {1..10}; do
-    curl -sS -m 1 ${MSB_IP}:8086 && curl -sS -m 1 ${MSB_IP}:80 && break
+    curl -sS -m 1 ${MSB_IP}:8086 && curl -sS -m 1 ${MSB_IP}:80 && curl -sS -m 1 ${ACTIVEMQ_IP}:8161 && break
     echo sleep $i
     sleep $i
 done
+
+
+curl -H "Content-Type: application/json" -X POST -d '{"serviceName": "ActiveMQ","protocol": "TCP","nodes": [{"ip": "'${ACTIVEMQ_IP}'","port": "61616"}]}' http://${MSB_IP}/openoapi/microservices/v1/services
 
 # Pass any variables required by Robot test suites in ROBOT_VARIABLES
 ROBOT_VARIABLES="-v MSB_IP:${MSB_IP}"
--- test/csit/plans/common-services-message-broker/sanity-check/teardown.sh
+++ test/csit/plans/common-services-message-broker/sanity-check/teardown.sh
@@ -17,3 +17,4 @@
 # This script is sourced by run-csit.sh after Robot test completion.
 
 kill-instance.sh i-msb
+kill-instance.sh i-activemq
