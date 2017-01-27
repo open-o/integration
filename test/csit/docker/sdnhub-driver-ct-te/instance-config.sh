51a8472cd597f306459c75ccafd68834ace433d7
Gary Wu <gary.i.wu@huawei.com>
2017-01-27 11:50:37 -0800

    Moved sdno-driver docker scripts to sdnhub
    Change-Id: Ic1ee53fd1693fb2c9e8e10e93d1386eb1497501d
    Issue-id: SDNHUB-36
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/docker/sdnhub-driver-ct-te/instance-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+sed -i "s|SDNO_DRIVER_CT_TE_ADDRESS=.*:|SDNO_DRIVER_CT_TE_ADDRESS=\"$SERVICE_IP:|" run.sh
+sed -i "s|MSB_ADDRESS=.*|MSB_ADDRESS=$MSB_ADDR|" run.sh
+cat run.sh
---
+++ test/csit/docker/sdnhub-driver-huawei-openstack/instance-workaround.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# fix broken start/stop scripts
+sed -i '1i#!/bin/bash'
+dos2unix bin/{start.sh,stop.sh}
+
+# make catalina.sh found
+sed -i '/#!\/bin\/bash/a export PATH=$PATH:/service/bin' /service/bin/{start.sh,stop.sh}
+# workaround for tomcat blocked by VM always short of entropy
+sed -i '/#!\/bin\/bash/a export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"' /service/bin/start.sh
+
+# code stick to 127.0.0.1:8080, socat proxy it to actual MSB
+sed -i 's/"msb.openo.org"/"127.0.0.1"/g;s/"port":"80"/"port":"8080"/g' /service/etc/conf/restclient.json
+
+# replace empty ip address with service ip, provided from command line while running docker
+sed -i "s/\"ip\": \"\"/\"ip\": \"$SERVICE_IP\"/g" /service/generalconfig/driver.json
+
+# done
