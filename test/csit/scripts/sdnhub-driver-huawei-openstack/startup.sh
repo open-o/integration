b2d18c1f07a1175adde768c932ff329800d3fc5f
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-01-28 00:15:14 +0530

    Robot test cases for the SDNHUB drivers
    Robot test cases for the SDNHUB drivers:
        - sdnhub-driver-huawei-l3vpn
        - sdnhub-driver-huawei-openstack
        - sdnhub-driver-huawei-overlay
        - sdnhub-driver-huawei-servicechain
    
    Issue-Id: SDNHUB-38
    Change-Id: I8ee3dbae0207c40a18c026dda9100e89c35ef9e4
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ test/csit/scripts/sdnhub-driver-huawei-openstack/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# $1 nickname for the CATALOG instance
+# $2 IP address of MSB
+source ${SCRIPTS}/common_functions.sh
+echo $@
+
+#Start DRIVER HUAWEI OPENSTACK
+run-instance.sh openoint/sdnhub-driver-huawei-openstack d-driver-huawei-openstack " -i -t -e MSB_ADDR=$2"
+curl_path='http://'$2'/openoapi/drivermgr/v1/drivers'
+sleep_msg="Waiting_connection_of_url_for:"$1
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="\[\]" EXCLUDE_STRING
