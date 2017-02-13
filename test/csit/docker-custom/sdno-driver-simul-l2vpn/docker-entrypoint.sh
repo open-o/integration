e78c61597eb226f63e6349e4d6f9ca7af01a16a6
Viorel Mirea <viorel.mirea@huawei.com>
2017-02-13 14:57:06 +0000

    Simulated Docker l2vpn Driver
    Issue-id: SDNO-1284
    
    Change-Id: Iba12a4107c3f805cd3a79814d117fc4607e3af5d
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

---
+++ test/csit/docker-custom/sdno-driver-simul-l2vpn/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MOCO_JAR="moco-runner-0.11.0-standalone.jar"
+DRIVERMANAGER_URL="/openoapi/drivermgr/v1/drivers"
+
+L2VPN_TEMPLATE_FILE="template-l2vpn-driver.json"
+L2VPN_SIMU_FILE="l2vpn_sim_file.json"
+L2VPN_DRIVER_SIMU_FILENAME="L2vpnDriver.json"
+
+SERVICE_TYPE="http"
+DRIVER_LISTEN_PORT="8641";
+DRIVER_SHUTDOWN_PORT="18641";
+LOG_FILENAME="./logs/moco.log"
+
+mkdir -p logs
+
+DRIVER_IP=`ifconfig  eth0| grep "inet " | awk '{print $2}'`
+cat $L2VPN_TEMPLATE_FILE | sed -e 's/DRIVER_IP/$DRIVER_IP/g' > $L2VPN_SIMU_FILE
+
+#Register L2VPN Driver to Driver Manager
+curl -d @$L2VPN_SIMU_FILE -H "Content-Type: application/json;charset=UTF-8" http://$MSB_IP$DRIVERMANAGER_URL
+
+#Start the simulated driver for L2VPN
+java -jar $MOCO_JAR $SERVICE_TYPE -p $DRIVER_LISTEN_PORT  -s $DRIVER_SHUTDOWN_PORT -c  $L2VPN_DRIVER_SIMU_FILENAME | tee -a $LOG_FILENAME
+tail $LOG_FILENAME
