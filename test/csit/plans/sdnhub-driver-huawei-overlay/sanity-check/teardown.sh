387f3c36ea6e9f15fad17376bb7a33e81caba30e
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-02-15 19:58:45 +0530

    CSIT for overlay driver
    CSIT for overlay driver.
    Modify port number for SDN controller.
    
    Change-Id: I864b09ff33360f7e4856db93dcb8ce511476a97a
    Issue-Id: SDNHUB-59
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- test/csit/plans/sdnhub-driver-huawei-overlay/sanity-check/teardown.sh
+++ test/csit/plans/sdnhub-driver-huawei-overlay/sanity-check/teardown.sh
@@ -17,7 +17,7 @@
 # This script is sourced by run-csit.sh after Robot test completion.
 
 #kill-instance.sh i-brs
-kill-instance.sh i-mss
+#kill-instance.sh i-mss
 kill-instance.sh i-msb
 kill-instance.sh i-common-services-extsys
 kill-instance.sh d-drivermgr
