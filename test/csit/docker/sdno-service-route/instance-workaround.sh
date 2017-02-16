79512ed5ee621481ed6c22b1c210ddb7fe2b480c
chuwei <chuwei@huawei.com>
2017-02-16 11:01:42 +0800

    Update docker container for route service
    Update docker container for route service.
    
    SDNO-1123
    
    Change-Id: I9c98c96c37d072b0f32ff10b6f4ca3c06f3c59ee
    Signed-off-by: chuwei <chuwei@huawei.com>

--- test/csit/docker/sdno-service-route/instance-workaround.sh
+++ test/csit/docker/sdno-service-route/instance-workaround.sh
@@ -15,3 +15,15 @@
 # limitations under the License.
 #
 
+# make catalina.sh found
+sed -i '/#!\/bin\/bash/a export PATH=$PATH:/service/bin' /service/bin/{start.sh,stop.sh}
+# workaround for tomcat blocked by VM always short of entropy
+sed -i '/#!\/bin\/bash/a export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"' /service/bin/start.sh
+
+# code stick to 127.0.0.1:8080, socat proxy it to actual MSB
+sed -i 's/"msb.openo.org"/"127.0.0.1"/g;s/"port":"80"/"port":"8080"/g' /service/etc/conf/restclient.json
+
+# replace empty ip address with service ip, provided from command line while running docker
+sed -i "s/\"ip\": \"\"/\"ip\": \"$SERVICE_IP\"/g" /service/generalconfig/msbRegistion.json
+
+# done
