28ab9ff6bc846f304bd26314aaf0f74327a0475e
Viorel Mirea <viorel.mirea@huawei.com>
2017-02-22 16:41:45 +0000

    Renamed sdnhub-driver-huawei-openstack
    Issue-id: SDNO-1321
    
    Change-Id: I50bc34d6235abcea8e9df66bfbd2055e8976f5ab
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

---
+++ test/csit/docker-custom/simulate-sdnhub-driver-huawei-openstack/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+MOCO_JAR="moco-runner-0.11.0-standalone.jar"
+DRIVERMANAGER_URL="/openoapi/drivermgr/v1/drivers"
+
+OVERLAY_TEMPLATE_FILE="template-openstack-driver.json"
+OVERLAY_SIMU_FILE="openstack_sim_file.json"
+OVERLAY_DRIVER_SIMU_FILENAME="sdnhub-driver-huawei-openstack.json"
+
+SERVICE_TYPE="http"
+DRIVER_LISTEN_PORT="8539";
+DRIVER_SHUTDOWN_PORT="18539";
+LOG_FILENAME="./logs/moco.log"
+
+mkdir -p logs
+
+DRIVER_IP=`ifconfig  eth0| grep "inet " | awk '{print $2}'`
+cat $OVERLAY_TEMPLATE_FILE | sed -e 's/DRIVER_IP/$DRIVER_IP/g' > $OVERLAY_SIMU_FILE
+
+#Register OVERLAY Driver to Driver Manager
+curl -d @$OVERLAY_SIMU_FILE -H "Content-Type: application/json;charset=UTF-8" http://$MSB_ADDR$DRIVERMANAGER_URL
+
+#Start the simulated driver for OPENSTACK
+java -jar $MOCO_JAR $SERVICE_TYPE -p $DRIVER_LISTEN_PORT  -s $DRIVER_SHUTDOWN_PORT -c  $OVERLAY_DRIVER_SIMU_FILENAME | tee -a $LOG_FILENAME
+tail $LOG_FILENAME
