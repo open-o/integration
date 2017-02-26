94cac06c4c156b5a725deec17181310617622b56
fujinhua <fu.jinhua@zte.com.cn>
2017-02-26 12:53:00 +0800

    Fix env variables of nfvo-gvnfmdriver
    Change-Id: Ibd6a6092df342582f458d086a26a4a1f7b1b46a5
    Issue-Id: INT-173
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- test/csit/docker/nfvo-driver-vnfm-gvnfm/instance-config.sh
+++ test/csit/docker/nfvo-driver-vnfm-gvnfm/instance-config.sh
@@ -17,13 +17,11 @@
 # Configure MSB IP address
 MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
-GVNFM_DRIVER_PATH = nfvo/drivers/vnfm/gvnfm/gvnfmadapter
-GVNFM_DRIVER_CONFIG_PATH = nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config
 
-sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" GVNFM_DRIVER_CONFIG_PATH/config.py
-sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" GVNFM_DRIVER_CONFIG_PATH/config.py
-sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" GVNFM_DRIVER_CONFIG_PATH/config.py
+sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
+sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
+sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 cat GVNFM_DRIVER_CONFIG_PATH/config.py
 
-sed -i "s|127\.0\.0\.1|$SERVICE_IP|" GVNFM_DRIVER_PATH/run.sh
-sed -i "s|127\.0\.0\.1|$SERVICE_IP|" GVNFM_DRIVER_PATH/stop.sh
+sed -i "s|127\.0\.0\.1|$SERVICE_IP|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/run.sh
+sed -i "s|127\.0\.0\.1|$SERVICE_IP|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/stop.sh
