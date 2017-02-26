49b66fb6be7dff170a8ab179ec69229c670ec427
fujinhua <fu.jinhua@zte.com.cn>
2017-02-26 12:31:19 +0800

    Add nslcm,ztevnfmdriver,gvnfmdriver CSIT use cases
    Change-Id: Ie48fa3140cff371e38e8491b4d04fc203b3b28a3
    Issue-Id: INT-173
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ test/csit/plans/nfvo/lcm-sanity-check/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Place the scripts in run order:
+# Start all process required for executing test case
+
+source ${SCRIPTS}/common_functions.sh
+
+# Start MSB
+${SCRIPTS}/common-services-microservice-bus/startup.sh i-msb
+MSB_IP=`get-instance-ip.sh i-msb`
+curl_path='http://'${MSB_IP}'/openoui/microservices/index.html'
+sleep_msg="Waiting_connection_for_url_for:i-msb"
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="org_openo_msb_route_title" REPEAT_NUMBER="15"
+
+# Start nslcm
+run-instance.sh openoint/nfvo-lcm nslcm " -i -t -e MSB_ADDR=${MSB_IP}:80"
+NSLCM_IP=`get-instance-ip.sh nslcm`
+sleep_msg="Waiting_for_nslcm"
+curl_path='http://'${NSLCM_IP}':8403/openoapi/nslcm/v1/swagger.json'
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' REPEAT_NUMBER=25 GREP_STRING="swagger"
+
+echo SCRIPTS
+# Pass any variables required by Robot test suites in ROBOT_VARIABLES
+ROBOT_VARIABLES="-v MSB_IP:${MSB_IP} -v NSLCM_IP:${NSLCM_IP} -v SCRIPTS:${SCRIPTS}"
+
