b0e8dafbb30d090358aad65b8e1e6f41ebd3a108
Viorel Mirea <viorel.mirea@huawei.com>
2017-03-22 10:57:59 +0000

    Increased wait time to 15 min
    Issue-id: SDNO-1458
    
    Change-Id: I59941661c8874de9188cb96130f9525bf3ad600f
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

--- test/csit/scripts/sdno-brs/startup.sh
+++ test/csit/scripts/sdno-brs/startup.sh
@@ -23,11 +23,10 @@ echo $@
 run-instance.sh openoint/sdno-service-mss i-mss "-e MSB_ADDR=$2"
 curl_path='http://'$2'/openoapi/microservices/v1/services/sdnomss/version/v1'
 sleep_msg="Waiting_connection_for_url_for:i-mss"
-wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="microservice not found"  REPEAT_NUMBER="30" MAX_TIME="60" STATUS_CODE="200" EXCLUDE_STRING
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="microservice not found" REPEAT_NUMBER="30" MAX_TIME="60" STATUS_CODE="200" EXCLUDE_STRING
 
 #Start BRS
 run-instance.sh openoint/sdno-service-brs $1 " -i -t -e MSB_ADDR=$2"
 curl_path='http://'$2'/openoapi/microservices/v1/services/sdnobrs/version/v1'
 sleep_msg="Waiting_connection_for_url_for:"$1
-#wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="serviceName" REPEAT_NUMBER="50" MAX_TIME="60"
-wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="microservice not found" REPEAT_NUMBER="50" MAX_TIME="60" STATUS_CODE="200" EXCLUDE_STRING
+wait_curl_driver CURL_COMMAND=$curl_path WAIT_MESSAGE='"$sleep_msg"' GREP_STRING="microservice not found" REPEAT_NUMBER="30" MAX_TIME="60" STATUS_CODE="200" EXCLUDE_STRING
