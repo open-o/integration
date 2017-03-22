ecbde5885e2fab0dde2af6037afe0bd702a3da8f
Xiuchao Wu <xiuchao.wu@huawei.com>
2017-03-22 09:38:16 +0000

    Auto-extract template id and sync with template
    Issue-id: SDNO-1471
    
    Change-Id: I6a28de08bb4cc7bb58e24d02742b702f4f32fb1d
    Signed-off-by: Xiuchao Wu <xiuchao.wu@huawei.com>

--- test/csit/scripts/sdno-lcm/creation-instantiation/create-ns.pl
+++ test/csit/scripts/sdno-lcm/creation-instantiation/create-ns.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-use JSON qw( decode_json );
+use JSON qw( encode_json decode_json );
 
 #read file
 sub readFile {
@@ -13,22 +13,40 @@ sub readFile {
     return $content;
 }
 
-#MSB address and the json file with request body.
+####MSB address and the json file with request body.
 $MSB_ADDR=$ARGV[0];
 $BODY_FILE_NAME=$ARGV[1];
 
 
-#Read creation request from JSON file
-my $body = readFile($BODY_FILE_NAME);
+##############################Get All Templates from Catalog ################
+my $extract_command = "curl -s http://".$MSB_ADDR."/openoapi/catalog/v1/servicetemplates";
+my $templates_response = `$extract_command`;
+my $all_templates = decode_json($templates_response);
 
-#prepare curl command. quiet mode is used since we use stdout to return the created service id.
-my $body_in_curl = "'".$body."'";
+
+##############################Prepare Creation Request&Command #############
+#Read creation request from JSON file
+my $body = decode_json(readFile($BODY_FILE_NAME));
+my $template_name = $body->{"nsdId"};
+
+for my $template (@$all_templates) {
+    my $name = $template->{"id"};
+    if($name eq $template_name) {
+        $body->{"nsdId"} = $template->{"serviceTemplateId"};
+        last;
+    }
+}
+
+####prepare curl command. quiet mode is used since we use stdout to return the created service id.
+my $body_in_curl = "'".encode_json($body)."'";
 my $creation_command = "curl -s -X POST -d ".$body_in_curl." -H 'Content-Type: application/json;charset=UTF-8' http://".$MSB_ADDR."/openoapi/sdnonslcm/v1/ns";
 
-#execute and get response
+
+
+#######################################Execute and Get Response###########
 my $response = `$creation_command`;
 my $rsp_json = decode_json($response);
 my $id = $rsp_json->{"nsInstanceId"};
 
-#return service instance id (needed by instantiation request)
+####return service instance id (needed by instantiation request)
 print $id;
