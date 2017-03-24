344e37d0e627d0057543115e57f1cd4126bd57da
Viorel Mirea <viorel.mirea@huawei.com>
2017-03-24 16:07:11 +0000

    Created swagger.json for simulated services
    Created swagger.json in mocco format
    Renamed the other json config file to general.json
    The main *-moco-config.json merges the separate json files
    
    Issue-id: SDNO-1488
    
    Change-Id: Iba8b2f611a3b455cec006fa930dd4d66414ad5a0
    Signed-off-by: Viorel Mirea <viorel.mirea@huawei.com>

--- test/csit/docker-custom/simulate-sdno-services/docker-entrypoint.sh
+++ test/csit/docker-custom/simulate-sdno-services/docker-entrypoint.sh
@@ -39,7 +39,7 @@ while [ $index -ne $array_size ]; do
     #Register to MSB services
     curl -d @$MSB_REG_JSON -H "Content-Type: application/json;charset=UTF-8" http://$MSB_ADDR$MSB_SERVICES_URL
     #Start Moco
-    java -jar ../$MOCO_JAR $SERVICE_TYPE -p $MICROSERVICE_LISTEN_PORT  -c  $JSON_COMPLETE | tee -a $PROJECT_NAME.log  ../common.log &
+    java -jar ../$MOCO_JAR $SERVICE_TYPE -p $MICROSERVICE_LISTEN_PORT  -g  $JSON_COMPLETE | tee -a $PROJECT_NAME.log  ../common.log &
     
     cd ..
     index=$((index + 1))
