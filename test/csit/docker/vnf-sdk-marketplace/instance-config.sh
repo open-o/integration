e9aed7e9c7dc9b127669fcdc38d1bf5ae9a62715
Murali-P <murali.p@huawei.com>
2017-04-05 09:53:39 +0530

    Docker file change
    Resolved:VNFSDK-21
    
    Change-Id: Ia0809c41a7a4a55eda021eaaed52d81a1f452870
    Signed-off-by: Murali-P <murali.p@huawei.com>

--- test/csit/docker/vnf-sdk-marketplace/instance-config.sh
+++ test/csit/docker/vnf-sdk-marketplace/instance-config.sh
@@ -16,16 +16,15 @@
 #
 
 # Configure MSB IP address
-sed -i "s|msb.address=.*|msb.address=$MSB_ADDR|" etc/microservice.ini
-cat etc/microservice.ini
-echo
+MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
+sed -i "s|msb\.openo\.org|${MSB_IP}|" etc/conf/restclient.json
+cat etc/conf/restclient.json
 
 # Configure self IP address for MSB
 sed -i "s|10\.229\.47\.199|$SERVICE_IP|" etc/microservice/marketplace_rest.json
 cat etc/microservice/marketplace_rest.json
 echo
 
-MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 sed -i "s|IP=.*|IP=$MSB_IP|" webapps/ROOT/WEB-INF/classes/marketplace.properties
 cat webapps/ROOT/WEB-INF/classes/marketplace.properties
 echo
