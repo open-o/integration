a56ec521d3ba29663ed0dfd3cf8f0580d0b12491
fujinhua <fu.jinhua@zte.com.cn>
2017-04-05 09:43:16 +0800

    Fix gvnfm docker config script
    Change-Id: Id5d5a2bc9920b2e2f33aa5edaaafbd8485412354
    Issue-Id: INT-216
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- test/csit/docker/nfvo-driver-vnfm-gvnfm/instance-config.sh
+++ test/csit/docker/nfvo-driver-vnfm-gvnfm/instance-config.sh
@@ -21,7 +21,7 @@ MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
-cat GVNFM_DRIVER_CONFIG_PATH/config.py
+cat nfvo/drivers/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 
 sed -i "s|127\.0\.0\.1|$SERVICE_IP|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/run.sh
 sed -i "s|127\.0\.0\.1|$SERVICE_IP|" nfvo/drivers/vnfm/gvnfm/gvnfmadapter/stop.sh
